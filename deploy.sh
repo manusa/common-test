37db1872229e622235d30c4fe3963bc150caea59
Marc Nuri <marc@marcnuri.com>
2018-05-25 00:00:29 +0200

    CI: Deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo "Deploy script running"
 if [[ ( "$TRAVIS_BRANCH" = 'master'  ||  "$TRAVIS_BRANCH" = 'develop'  ) && ( "$TRAVIS_PULL_REQUEST" == 'false' ) ]]; then
   echo "Running gradlew uploadArchives"
   openssl aes-256-cbc -K $encrypted_11d0b920ac0b_key -iv $encrypted_11d0b920ac0b_iv -in travis.gpg.enc -out travis.gpg -d
-  ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=travis.gpg.enc
+  ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=travis.gpg || exit 1
 else
   echo "Branch not deployable -> exiting"
 fi
